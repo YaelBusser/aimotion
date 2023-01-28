16fb7c2f4dccb16b8ee2b42ca5c76651e663f685
Yaël Busser <mrmaclevine@gmail.com>
2023-01-28 02:53:45 +0100

    Début ajout form inscr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
-
+    <title>{{ config('app.name', 'Aimotion') }}</title>
+    <link rel="icon" href="{{asset('images/logo.jpg')}}" type="image/x-icon" />
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Aimotion') }}</title>
-        <link rel="icon" href="{{asset('favicon.ico')}}" type="image/x-icon" />
+        <link rel="icon" href="{{asset('images/logo.jpg')}}" type="image/x-icon" />
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
         <link href="https://unpkg.com/tailwindcss@^2.0/dist/tailwind.min.css" rel="stylesheet">
